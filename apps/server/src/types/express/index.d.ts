7114ac1481556b9fd792dec9d82a95eae2d1b4c4
ManasArora33 <manasarora33179@gmail.com>
2025-08-17 18:24:37 +0530

    Add custom TypeScript declarations

--- apps/server/src/types/express/index.d.ts
+++ apps/server/src/types/express/index.d.ts
@@ -13,3 +13,11 @@ declare global {
     }
   }
 }
+
+declare module 'passport-github2';
+declare module 'passport-google-oauth20';
+declare module 'passport';
+declare module 'bcrypt';
+declare module 'jsonwebtoken';
+declare module 'cors';
+declare module 'cookie-parser';
