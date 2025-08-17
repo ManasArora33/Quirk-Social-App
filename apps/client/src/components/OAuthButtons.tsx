d14dfbf9cf3092849f8ac18b4dcf36402febc372
ManasArora33 <manasarora33179@gmail.com>
2025-08-17 19:28:46 +0530

    updated base url in oauth buttons

--- apps/client/src/components/OAuthButtons.tsx
+++ apps/client/src/components/OAuthButtons.tsx
@@ -25,7 +25,7 @@ export const OAuthButtons = () => {
   return (
     <div className="flex flex-col gap-4 mt-6">
       <button 
-        onClick={() => window.open(`${base}/api/v1/oauth/google`, 'oauth', 'width=500,height=600')}
+        onClick={() => window.open(`${base}/oauth/google`, 'oauth', 'width=500,height=600')}
         className="w-full flex gap-3 items-center justify-center py-2.5 border border-gray-700 rounded-full hover:bg-gray-900 transition-all transform hover:scale-105 active:scale-95 font-semibold"
       >
         <FcGoogle size={22} />
@@ -33,7 +33,7 @@ export const OAuthButtons = () => {
       </button>
       
       <button
-        onClick={() => window.open(`${base}/api/v1/oauth/github`, 'oauth', 'width=500,height=600')}
+        onClick={() => window.open(`${base}/oauth/github`, 'oauth', 'width=500,height=600')}
         className="w-full flex gap-3 items-center justify-center py-2.5 border border-gray-700 rounded-full hover:bg-gray-900 transition-all transform hover:scale-105 active:scale-95 font-semibold"
       >
         <FaGithub size={22} />
