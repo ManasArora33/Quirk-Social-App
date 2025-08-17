21dc4013ab7c2aae5166172c5363321c8db5e242
ManasArora33 <manasarora33179@gmail.com>
2025-08-17 18:26:47 +0530

    added custom type declarations correctly

---
+++ apps/server/src/types/custom.d.ts
@@ -0,0 +1,7 @@
+declare module 'passport-github2';
+declare module 'passport-google-oauth20';
+declare module 'passport';
+declare module 'bcrypt';
+declare module 'jsonwebtoken';
+declare module 'cors';
+declare module 'cookie-parser';
--- apps/server/src/types/express/index.d.ts
+++ apps/server/src/types/express/index.d.ts
@@ -14,10 +14,3 @@ declare global {
   }
 }
 
-declare module 'passport-github2';
-declare module 'passport-google-oauth20';
-declare module 'passport';
-declare module 'bcrypt';
-declare module 'jsonwebtoken';
-declare module 'cors';
-declare module 'cookie-parser';
